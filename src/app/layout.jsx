ef3d30e5461533e3f2832a69cdad4a77125b108b
gantrol <501963347@qq.com>
2025-03-20 12:11:48 +0800

    typo: 修改部分描述性文本

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -5,7 +5,7 @@ import { LanguageProvider } from '@/lib/i18n/LanguageContext';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: '猫猫贴纸：自动遮挡隐私',
+  title: '猫猫贴纸：AI遮挡隐私',
   description: '猫咪利用AI，用贴纸挡住你的隐私',
   icons: {
     icon: [
